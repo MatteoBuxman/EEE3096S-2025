c11c3935ab3f4f9864f5ae7f74273d93daebcaf4
MatteoBuxman <buxmanm@icloud.com>
2025-09-17 23:14:26 +0200

    Merged in Practical3 directory

---
+++ Practical2/Core/Src/topdf.py
@@ -0,0 +1,19 @@
+from fpdf import FPDF #Install the library using pip if you do not have it
+
+# Load the contents of the main.c file
+with open("assembly.s", "r") as file:
+    c_code = file.read()
+
+# Initialize PDF
+pdf = FPDF()
+pdf.set_auto_page_break(auto=True, margin=15)
+pdf.add_page()
+pdf.set_font("Courier", size=10)
+
+# Split the code into lines and add to PDF
+for line in c_code.splitlines():
+    pdf.cell(0, 5, txt=line, ln=True)
+
+# Save the PDF
+output_path = "main.pdf"
+pdf.output(output_path)
